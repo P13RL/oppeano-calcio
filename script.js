e2c56c79f4a0939aff8ac1b7818728685c553588
P13RL <ambrosopierlorenzo@gmail.com>
2025-07-16 09:56:27 +0200

    Primo test pagina completa

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const sponsors = [
-  "img/sponsor/2.jpeg",
-  "img/sponsor/3.jpeg"
+  "img/sponsor/Affiliazione_Logo_Oppeano.png",
+  "img/sponsor/Ballottin Luciano Sas.jpg",
+  "img/sponsor/Banca_Veronese.jpg",
+  "img/sponsor/Bevande_Verona.png",
+  "img/sponsor/Carpenteria_guerra.png",
+  "img/sponsor/Centomo_logo.png",
+  "img/sponsor/citres.png",
+  "img/sponsor/Doctor_Glass.jpg",
+  "img/sponsor/eurospin.jpg",
+  "img/sponsor/floor_green.jpg",
+  "img/sponsor/green_company.jpg",
+  "img/sponsor/guariento_autolavaggio.png",
+  "img/sponsor/immobiliare_la_corte.jpeg",
+  "img/sponsor/kasba_viaggi.png",
+  "img/sponsor/littlelfuse.jpg",
+  "img/sponsor/logistica_1.jpg",
+  "img/sponsor/Makeroni.png"
 ];
 
 let current = 0;
